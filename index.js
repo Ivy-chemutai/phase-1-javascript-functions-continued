37e2013b5c706439501729538ddbc934bef48804
ivy <ivy.chemutai@student.moringaschool.com>
2025-06-09 15:34:49 +0300

    JavaScript Lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+ function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+  return "roller-skate";
+
+  const saturdayFun = function(activity) {
+    console.log(`This Saturday, I want to ${activity}!`);
+  };
+
+ }
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+    return "go to the office";
+    const mondayWork = function(activity) {
+      console.log(`This Monday, I will ${activity}.`);
+    }
+}
+function wrapAdjective(flair = "*") {
+  return function(adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+    return "You are *special*!";
+
+      return function(adjective) {
+        const encouragingPromptFunction = wrapAdjective("!!!")
+     console.log(encouragingPromptFunction("a dedicated programmer"));
+
+      };
+    };
+  };
